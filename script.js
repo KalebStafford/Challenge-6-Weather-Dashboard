c9f074a0b0586c036e23c5c595c1a5fa900aea20
Kaleb Stafford <kalebstafford0@gmail.com>
2022-05-22 12:57:50 -0500

    saving data

--- script.js
+++ script.js
@@ -13,3 +13,38 @@ let cityArray = [];
 if (window.localStorage.getItem("city")) {
   cityArray.push(...JSON.parse(window.localStorage.getItem("city")));
 }
+
+function historySave() {
+  historyContainerEl.innerHTML = "";
+  for (x = cityArray.length - 1; x > cityArray.length - 6; x--) {
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute("id", "prevHist");
+    newBtn.innerHTML = cityArray[x];
+    historyContainerEl.appendChild(newBtn);
+    // newBtn.classList.add('prevHist');
+  }
+}
+
+let prevHistBtn = document.getElementById("#prevHist");
+if (prevHistBtn) {
+  prevHistBtn.addEventListener("click", swapper, false, () => {
+    checkWeatherBtn();
+  });
+}
+
+function display(historyEl) {
+  let historyContainerEl = document.getElementById("#historyResult");
+  let historySpecial = localStorage.getItem(historyEl);
+  historyContainerEl.innerHTML = historySpecial;
+  citySearchEl.value = prevHistBtn((x = cityArray.length - 1));
+}
+
+// Save Data
+let presentCondition = {
+  id: "",
+  temp: "",
+  wind: "",
+  humidity: "",
+  UVIndex: "",
+  clouds: "",
+};
